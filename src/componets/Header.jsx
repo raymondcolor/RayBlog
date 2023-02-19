44091382cc49535f50094971d383898699808141
raymondcolor <sabiitiraymond29@gmail.com>
2023-02-19 22:19:16 +0300

    Add styles to header and search input

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import SearchInput from './SearchInput';
 const Header = () => {
   return (
-    <div>
-      <SearchInput />
+    <div className='headerContainer'>
+      <div className='headerSection'>
+        <h1>All What You Need In One Place</h1>
+        <p>
+          You can search for blogs about Health, Design, <br /> Software
+          development, Bussines, Afiliate marketing and more
+        </p>
+        <div className='inputSection'>
+          <SearchInput />
+        </div>
+      </div>
     </div>
   );
 };
--- src/componets/SearchInput.jsx
+++ src/componets/SearchInput.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState} from 'react';
 
 const SearchInput = () => {
-  return <div>SearchInput</div>;
+  const [keyword, setKeyword] = useState('');
+
+  return (
+    <div className='Input'>
+      <input
+        type='text'
+        placeholder='Search by category'
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <button>Search</button>
+    </div>
+  );
 };
 
 export default SearchInput;
