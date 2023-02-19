211995325ddfdfb4adb3a5a571b980284be8a6a3
raymondcolor <sabiitiraymond29@gmail.com>
2023-02-19 16:21:57 +0300

    Add pages in page folder,componet folder and creates routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Error from './Pages/Error';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';
+import ViewBlog from './Pages/ViewBlog';
 function App() {
   return (
     <div>
-      <h1>Hello world with SASS</h1>
+      <Router>
+        <Routes>
+          <Route path='*' element={<Error />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/viewblog' element={<ViewBlog />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
