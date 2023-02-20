b28203564f1c55d9536d3a35470536104cb09f74
raymondcolor <sabiitiraymond29@gmail.com>
2023-02-20 22:59:28 +0300

    Add log out function and changes to menu

--- src/componets/navBar/NavBar.jsx
+++ src/componets/navBar/NavBar.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './nav.scss';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {AuthContext} from '../../context/AuthContextProvider';
+import Avator from './avator.jpg';
+import {auth} from '../../firebase';
+import {signOut} from 'firebase/auth';
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const {currentUser} = useContext(AuthContext);
+  console.log(currentUser);
+  const SignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate('/');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
     <div>
       <div className='navContainer'>
@@ -11,10 +27,25 @@ const NavBar = () => {
             <h1>RayBlog</h1>
           </Link>
         </div>
-        <div className='menu'>
-          <Link to={'/login'}>Login In</Link>
-          <Link to={'/Signup'}>Sign Up</Link>
-        </div>
+        {currentUser ? (
+          <div className='menu'>
+            <Link to={'/'}>Blog</Link>
+            <Link to={'/blogpost'}>Create Blog</Link>
+            <button onClick={SignOut}>log out</button>
+            <div className='navProfile'>
+              {currentUser ? (
+                <img src={currentUser.photoURL} alt='' />
+              ) : (
+                <img src={Avator} alt='' />
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className='menu'>
+            <Link to={'/login'}>Login In</Link>
+            <Link to={'/Signup'}>Sign Up</Link>
+          </div>
+        )}
       </div>
     </div>
   );
