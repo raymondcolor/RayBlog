de42acebe6af87b52292891e2bb8757dc109bd10
raymondcolor <sabiitiraymond29@gmail.com>
2023-02-21 01:12:25 +0300

    Add my blog page UI content and styles

--- src/componets/myBlogs/Header.jsx
+++ src/componets/myBlogs/Header.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import React, {useContext} from 'react';
+import './myBlogs.scss';
+import {AuthContext} from '../../context/AuthContextProvider';
+import {Link} from 'react-router-dom';
 const Header = () => {
-  return <div>Header</div>;
+  const {currentUser} = useContext(AuthContext);
+  return (
+    <div className='myBlogsHeader'>
+      <div className='headerContent'>
+        <div className='myblogsProfile'>
+          <img src={currentUser.photoURL} alt='' />
+        </div>
+        <h1>{currentUser.displayName}</h1>
+      </div>
+
+      <Link to={'/createblog'}>
+        <div className='addBtn'>
+          <p>+</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
