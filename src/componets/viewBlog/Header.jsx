a570e24529145b8091cc29d3b6ff56d81e9968c2
raymondcolor <sabiitiraymond29@gmail.com>
2023-02-22 21:41:02 +0300

    Add header ans it styles on the view blog page

--- src/componets/viewBlog/Header.jsx
+++ src/componets/viewBlog/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './viewblog.scss';
 
-
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className='viewblogHeader'>
+      <div className='headerContent'>
+        <h1>The Design Of everyday things jjsjfs jshfsdfs jskjdfs sjfsdf </h1>
+        <div className='auhorDetails'>
+          <div className='authorImage'>
+            <img
+              src='https://lh3.googleusercontent.com/a/AEdFTp7-HrKJeQtn3QYLWrtwlAOU6Vlk9ohzlti8-UHn=s96-c'
+              alt=''
+            />
+          </div>
+          <div>
+            <p>By: Sabiiti Raymond</p>
+            <span>12/2/2023</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
