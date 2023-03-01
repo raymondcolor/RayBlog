89ee612739080faae9697fa62a1f1d8a12f17f2d
raymondcolor <sabiitiraymond29@gmail.com>
2023-03-02 00:13:41 +0300

    Change in app comfig and my blog css

--- src/firebase.js
+++ src/firebase.js
@@ -4,22 +4,22 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDyiyQNCIHeAiVgqPaU-J9Jvs88mv8uIZA',
-  authDomain: 'raychat1-59420.firebaseapp.com',
-  projectId: 'raychat1-59420',
-  storageBucket: 'raychat1-59420.appspot.com',
-  messagingSenderId: '273558404586',
-  appId: '1:273558404586:web:7113c57bc81a6bc2dcc693',
-  measurementId: 'G-VEK40BW2LG',
+  apiKey: 'AIzaSyBzfgeX5L--nuhA1jqZo9uFdLS3XlXhPrg',
+  authDomain: 'rayblog-a3a5d.firebaseapp.com',
+  projectId: 'rayblog-a3a5d',
+  storageBucket: 'rayblog-a3a5d.appspot.com',
+  messagingSenderId: '1073334566649',
+  appId: '1:1073334566649:web:f41f7eb74c2faee5caec28',
+  measurementId: 'G-9JKL5TE4SB',
 };
 // const firebaseConfig = {
-//   apiKey: 'AIzaSyB7wVcFz5SYVb9uyj8uSGS4h65Es3xHEB8',
-//   authDomain: 'rayblog-8a1e3.firebaseapp.com',
-//   projectId: 'rayblog-8a1e3',
-//   storageBucket: 'rayblog-8a1e3.appspot.com',
-//   messagingSenderId: '182754878260',
-//   appId: '1:182754878260:web:be21525115d03f505bfb61',
-//   measurementId: 'G-GVPKZK80NC',
+//   apiKey: 'AIzaSyDyiyQNCIHeAiVgqPaU-J9Jvs88mv8uIZA',
+//   authDomain: 'raychat1-59420.firebaseapp.com',
+//   projectId: 'raychat1-59420',
+//   storageBucket: 'raychat1-59420.appspot.com',
+//   messagingSenderId: '273558404586',
+//   appId: '1:273558404586:web:7113c57bc81a6bc2dcc693',
+//   measurementId: 'G-VEK40BW2LG',
 // };
 
 const app = initializeApp(firebaseConfig);
